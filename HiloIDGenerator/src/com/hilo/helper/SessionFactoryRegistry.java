0b7261f0fe123881a9a63785cad80d4bbb08fe50
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-03-06 12:57:19 +0530

    SessionFactory Service Registry

---
+++ HiloIDGenerator/src/com/hilo/helper/SessionFactoryRegistry.java
@@ -0,0 +1,28 @@
+package com.hilo.helper;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryRegistry {
+	private static SessionFactory sessionFactory = null;
+
+	static {
+		Configuration configuration = new Configuration().configure("hibernate-mysql.cfg.xml");
+		try {
+			sessionFactory = configuration.buildSessionFactory();
+		} catch (Throwable t) {
+			t.printStackTrace();
+			throw t;
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void close() {
+		if (sessionFactory != null) {
+			sessionFactory.close();
+		}
+	}
+}
